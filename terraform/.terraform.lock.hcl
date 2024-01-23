f9e7581522fa3179aa7a763ed02c9c2ab35542ee
usernaimandrey <anshlyapnikov@yandex.ru>
2024-01-23 16:17:51 +0300

    add base structure

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.106.0"
+  hashes = [
+    "h1:jHMQuc/PzkR31x8qgm5sM6D5yhN0X/NwkbNJCZZjWPE=",
+  ]
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token                    = var.y_token
+  cloud_id                 = var.y_cloud_id
+  folder_id                = var.y_folder_id
+  zone                     = var.y_zone
+}
